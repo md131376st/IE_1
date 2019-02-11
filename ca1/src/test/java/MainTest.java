ea11d7b35f4fe1a06efdeb7b7211a5506008bfdd
Negar <kashefnegar@gmail.com>
2019-02-11 10:52:12 +0330

    MainTest created

--- ca1/src/test/java/MainTest.java
+++ ca1/src/test/java/MainTest.java
@@ -1,2 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class MainTest {
+    @Test
+    public void testSomething(){
+        System.out.println("test something");
+    }
+
+    @Test
+    public void testOtherthing(){
+        System.out.println("test Otherthing");
+    }
 }
