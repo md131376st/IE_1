0161a3b3556f1f1e7cf1b3ddc0d73125177fdc13
Negar <kashefnegar@gmail.com>
2019-02-22 19:06:01 +0330

    Added Temporary User

--- ca1/src/main/java/Main.java
+++ ca1/src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args)throws Exception {
         MyUser myUser=MyUser.getInstance();
-        addTempUser();
+        myUser.addTempUser();
         Projects myProjects = Projects.getInstance();
         myUser.adduser(new Utility().getDefultUser());
         AllSkills incSkills = AllSkills.getInstance();
@@ -21,21 +21,5 @@ public class Main {
 
         ReflectionServer server = new ReflectionServer();
         server.startServer();
-
-    }
-    private static void addTempUser(){
-        ArrayList<Skills> Skill = null;
-        Skills first = new Skills("HTML" , 5 );
-        Skills second = new Skills("JavaScript" , 4 );
-        Skills third = new Skills("C++" , 2 );
-        Skills forth = new Skills("Java" , 3 );
-
-        Skill.add(first);
-        Skill.add(second);
-        Skill.add(third);
-        Skill.add(forth);
-
-        Register tempUser = new Register("1" , "Ali" , "SharifZade" , "WebProgrammer"
-                                            ,"",Skill,":)");
     }
 }
--- ca1/src/main/java/MyUser.java
+++ ca1/src/main/java/MyUser.java
@@ -32,5 +32,22 @@ class MyUser {
                     return i;
             }
             return -1;
-        }
+    }
+    public static void addTempUser(){
+        ArrayList<Skills> Skill = null;
+        Skills first = new Skills("HTML" , 5 );
+        Skills second = new Skills("JavaScript" , 4 );
+        Skills third = new Skills("C++" , 2 );
+        Skills forth = new Skills("Java" , 3 );
+
+        Skill.add(first);
+        Skill.add(second);
+        Skill.add(third);
+        Skill.add(forth);
+
+        Register tempUser = new Register("1" , "Ali" , "SharifZade" , "WebProgrammer"
+                ,"",Skill,":)");
+
+        ourInstance.adduser(tempUser);
+    }
 }
