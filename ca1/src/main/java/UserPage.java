b3d349102f596ef2da8de1c54989870ac9080658
Negar <kashefnegar@gmail.com>
2019-02-22 22:13:25 +0330

    Selected User Info Done

--- ca1/src/main/java/UserPage.java
+++ ca1/src/main/java/UserPage.java
@@ -8,6 +8,21 @@ public class UserPage implements Page {
     String response = "";
     @Override
     public void HandleRequest(HttpExchange httpExchange) throws IOException {
+        String[] path = httpExchange.getRequestURI().getPath().split("/");
+        String id = path[3];
+
+        Register user = MyUser.getInstance().FindUser(id);
+        response = "<html><title>Project Info</title>"
+                + "<body>"
+                +"<ul>"
+                +   "<li>id = " + user.getId() + "</li>"
+                +   "<li>first name = " + user.getFirstName() + "</li>"
+                +   "<li>last name = " + user.getLastName() + "</li>"
+                +   "<li>job title = " + user.getJobTitle() + "</li>"
+                +   "<li>bio = " + user.getBio() + "</li>"
+                + "</ul> "
+                +"</body>"
+                + "</html>";
         httpExchange.sendResponseHeaders(200, response.length());
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
