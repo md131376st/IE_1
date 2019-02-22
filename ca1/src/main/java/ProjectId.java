028d0187e170c0c9f4f87f0054fed7e75981ba46
Negar <kashefnegar@gmail.com>
2019-02-22 21:39:30 +0330

    selecting project page checked availability of request

--- ca1/src/main/java/ProjectId.java
+++ ca1/src/main/java/ProjectId.java
@@ -12,12 +12,33 @@ public class ProjectId implements Page {
 
     @Override
     public void HandleRequest(HttpExchange httpExchange) throws IOException {
-        String response ="";
+        boolean hasNeeded;
+        String response;
+        response = "";
+        String[] path = httpExchange.getRequestURI().getPath().split("/");
+        String id = path[3];
+        hasNeeded = CheckAvailability(id);
+        if(hasNeeded)
+        {
+            httpExchange.sendResponseHeaders(200, response.length());
+        }
 
-        httpExchange.sendResponseHeaders(200, response.length());
+        else{
+            response = "<html>"
+                    + "<body><h2>You don't have enough requirement</body>"
+                    + "</html>";
+            httpExchange.sendResponseHeaders(403, response.length());
+        }
+
         OutputStream os = httpExchange.getResponseBody();
         os.write(response.getBytes());
         os.close();
+    }
+
+    private boolean CheckAvailability(String id) {
+        Projects myProjects = Projects.getInstance();
+        Register myUser=MyUser.getInstance().getMyusers_reg(0);
+        return myProjects.hasNecessarySkills(id,myUser);
     }
 
 }
